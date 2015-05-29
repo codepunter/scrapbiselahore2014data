78b0385e68a404ff78397b3aa8b2fdb32221ea7f
codepunter <codepunter@users.noreply.github.com>
2015-05-29 16:08:55 +0530

    Update scrap.py
    Updated with details of particular student

--- scrap.py
+++ scrap.py
@@ -9,11 +9,36 @@ rollNo=[]
 for tr in a:
 	td=tr.find_all("td")
 	rollNo.append(td[1].text.strip())
-print rollNo
-print "@@@@@@@@@@@@@@"
-print rollNo[0] 
+# print rollNo
+# print "@@@@@@@@@@@@@@"
+# print rollNo[0] 
 
 
 header1, status1 = subprocess.Popen(['curl', '--data','student_rno='+rollNo[0]+'', 'http://biselahore.com/hsscResult_2014.php'], stdout=subprocess.PIPE).communicate()
 
 print header1
+soup1=BeautifulSoup(header1)
+a1=soup1.find_all('td', {'width':"20%"})
+ft=[]
+for fThree in a1:
+	ft.append(fThree.text.strip())
+print ft
+
+
+
+
+candAndfatherNameSearch=soup1.find_all('td', {"class":"pagetext","align":"left","style":"font-size:12px"})
+candName=candAndfatherNameSearch[0].text.strip()
+fatherName=candAndfatherNameSearch[1].text.strip()
+print candName
+print fatherName
+
+
+collegeNameSearch=soup1.find_all('td', {"rowspan":"3","style":"font-size:12px"})
+collegeName=collegeNameSearch[0].text.strip()
+print collegeName
+
+
+
+
+# D:\MSDEV\PACS\dev_working_stable\main\Projects\RadSpeed\Build_Script\RIS-PACS
