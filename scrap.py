2bf928895a41eb2579679b0abf15588109a1e7ff
codepunter <codepunter@users.noreply.github.com>
2015-06-01 12:19:41 +0530

    Update scrap.py
    add sleep time to control time of threading

--- scrap.py
+++ scrap.py
@@ -1,6 +1,7 @@
 import subprocess
 from bs4 import BeautifulSoup
 import threading, urllib2
+import time
 import Queue
 import pymongo
 from pymongo import MongoClient
@@ -35,9 +36,9 @@ def find_all_in_collection(collection):
 
 
 
-# print rollNo
-# print "@@@@@@@@@@@@@@"
-# print rollNo[0] 
+# #print rollNo
+# #print "@@@@@@@@@@@@@@"
+# #print rollNo[0] 
 
 
 def read_url(url, queue):
@@ -45,62 +46,64 @@ def read_url(url, queue):
 
 	data, status1 = subprocess.Popen(['curl', '--data','student_rno='+url+'', 'http://biselahore.com/hsscResult_2014.php'], stdout=subprocess.PIPE).communicate()
 
-	print data
-	soup1=BeautifulSoup(data)
-	a1=soup1.find_all('td', {'width':"20%"})
-	ft=[]
-	for fThree in a1:
-		ft.append(fThree.text.strip())
-	print ft
+	#print data
+	try:
+		soup1=BeautifulSoup(data)
+		a1=soup1.find_all('td', {'width':"20%"})
+		ft=[]
+		for fThree in a1:
+			ft.append(fThree.text.strip())
+		#print ft
 
 
 
 
-	candAndfatherNameSearch=soup1.find_all('td', {"class":"pagetext","align":"left","style":"font-size:12px"})
-	candName=candAndfatherNameSearch[0].text.strip()
-	fatherName=candAndfatherNameSearch[1].text.strip()
-	print candName
-	print fatherName
+		candAndfatherNameSearch=soup1.find_all('td', {"class":"pagetext","align":"left","style":"font-size:12px"})
+		candName=candAndfatherNameSearch[0].text.strip()
+		fatherName=candAndfatherNameSearch[1].text.strip()
+		#print candName
+		#print fatherName
 
 
-	collegeNameSearch=soup1.find_all('td', {"rowspan":"3","style":"font-size:12px"})
-	collegeName=collegeNameSearch[0].text.strip()
-	print collegeName
+		collegeNameSearch=soup1.find_all('td', {"rowspan":"3","style":"font-size:12px"})
+		collegeName=collegeNameSearch[0].text.strip()
+		#print collegeName
 
-	subjectSearch=soup1.find_all('td',{"class":"TextHeading"})
-	subjectArray=[]
-	for subject in subjectSearch:
-		 if len(subject.attrs) == 1 :
-		 	subjectArray.append(subject.text.strip())
-	print subjectArray
+		subjectSearch=soup1.find_all('td',{"class":"TextHeading"})
+		subjectArray=[]
+		for subject in subjectSearch:
+			 if len(subject.attrs) == 1 :
+			 	subjectArray.append(subject.text.strip())
+		#print subjectArray
 
-	markSearch=soup1.find_all('td',{"class":"pagetext","align":"center"})
-	markArray=[]
-	for mark in markSearch:
-		markArray.append(mark.text.strip())
-	print markArray
-	markArray1=[]
-	for x in xrange(3,len(markArray),5):
-		markArray1.append(markArray[x])
-	print markArray1
+		markSearch=soup1.find_all('td',{"class":"pagetext","align":"center"})
+		markArray=[]
+		for mark in markSearch:
+			markArray.append(mark.text.strip())
+		#print markArray
+		markArray1=[]
+		for x in xrange(3,len(markArray),5):
+			markArray1.append(markArray[x])
+		#print markArray1
 
-	markArray2=[]
-	for x1 in xrange(4,len(markArray),5):
-		markArray2.append(markArray[x1])
-	print markArray2
+		markArray2=[]
+		for x1 in xrange(4,len(markArray),5):
+			markArray2.append(markArray[x1])
+		#print markArray2
 
-	com=[]
-	for comb in range(0,len(subjectArray)):
-		com.append({"subject":subjectArray[comb],"totalmarks":markArray1[comb],"marks_status":markArray2[comb]})
+		com=[]
+		for comb in range(0,len(subjectArray)):
+			com.append({"subject":subjectArray[comb],"totalmarks":markArray1[comb],"marks_status":markArray2[comb]})
 
-	print com
+		#print com
 
-	studentRecords={"RollNo":ft[0],"Reg_No":ft[2],"Result_Status":ft[3],"Name":candName,"Fname":fatherName,"collegeName":collegeName,"subject":com}
-	save_collection("studentRecords",studentRecords)
+		studentRecords={"RollNo":ft[0],"Reg_No":ft[2],"Result_Status":ft[3],"Name":candName,"Fname":fatherName,"collegeName":collegeName,"subject":com}
+		save_collection("studentRecords1",studentRecords)
 
-	print studentRecords
-	queue.put(data)
-
+		print studentRecords
+		queue.put(data)
+	except:
+		print "wait for sometime"
 
 
 
@@ -109,10 +112,11 @@ def fetch_parallel(urls):
 	result = Queue.Queue()
 	threads = [threading.Thread(target=read_url, args = (url,result)) for url in urls]
 	for t in threads:
-	    t.start()
+		time.sleep(2)
+		t.start()
 
 	for t in threads:
-	    t.join()
+		t.join()
 
 
 	return result
@@ -121,11 +125,12 @@ def fetch_parallel(urls):
 
 
 db=find_all_in_collection("clgdetails")
-print len(db[0]["clgdata"])
+#print len(db[0]["clgdata"])
 
-for x in range(0,len(db[0]['clgdata'])):
+for x in range(3,len(db[0]['clgdata'])):
 	header, status = subprocess.Popen(['curl', '--data','institute_code='+str(int(db[0]['clgdata'][x]['cid']))+'', 'http://biselahore.com/Zaidi_12th.php'], stdout=subprocess.PIPE).communicate()
-	print header
+	#print header
+	
 	soup=BeautifulSoup(header)
 	a=soup.table.tbody.find_all("tr")
 	rollNo=[]
